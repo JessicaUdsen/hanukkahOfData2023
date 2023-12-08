c5b5a492e7fe1a3af7274ea2d4a0285aa54d647c
Jessica <jpalardyudsen@gmail.com>
2023-12-08 12:56:30 +0100

    Initial commit and day 1

---
+++ 5784/day1.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+#See the problem statement here: https://hanukkah.bluebird.sh/5784/1/
+numberLetterKey <- list('a' = 2,
+                        'b' = 2,
+                        'c' = 2,
+                        'd' = 3,
+                        'e' = 3,
+                        'f' = 3,
+                        'g' = 4,
+                        'h' = 4,
+                        'i' = 4,
+                        'j' = 5,
+                        'k' = 5,
+                        'l' = 5,
+                        'm' = 6,
+                        'n' = 6,
+                        'o' = 6,
+                        'p' = 7,
+                        'q' = 7,
+                        'r' = 7,
+                        's' = 7,
+                        't' = 8,
+                        'u' = 8,
+                        'v' = 8,
+                        'w' = 9,
+                        'x' = 9,
+                        'y' = 9,
+                        'z' = 9)
+customers <- read_csv('noahs-customers.csv')
+
+letterToNum <- function(name){
+  nameVect <- str_split_1(name, '')
+  mapNums <- sapply(nameVect, function(x) numberLetterKey[x]) %>% unlist()
+  mapNumsStr <- paste(mapNums, collapse='')
+  return(mapNumsStr)
+}
+
+namePhoneData <- customers %>%
+  select(name, phone) %>%
+  mutate(phoneClean = str_remove_all(phone, pattern = '[^\\d]')) %>%
+  mutate(lastName = sapply(strsplit(name, ' '), function(x) tolower(x[2]))) %>%
+  mutate(lastNameNums = sapply(lastName, function(x) letterToNum(x))) %>%
+  filter(lastNameNums %in% phoneClean) 
---
+++ README.md
@@ -0,0 +1,10 @@
+# Hanukkah of Data 5784
+These are completed exercises from [Hanukkah of Data](https://hanukkah.bluebird.sh/5784/). 
+
+Hanukkah of Data (HoD) is similar to Advent of Code (AoC), but is more geared towards data science and data munging exercises (and is obviously Hanukkah themed!).
+Check out the About page [here](https://hanukkah.bluebird.sh/about/).
+
+This year, like with AoC, I'm using R. My specific goals:
+1. Improve my R skills in general
+2. Code in a way idiomatic to R
+3. Learn how to write code that is efficient, without sacrificing readability
